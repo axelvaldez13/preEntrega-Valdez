f87f63a87b48e510880543c60b2d1291baff2170
axelvaldez13 <axeel.hernan@gmail.com>
2023-03-04 15:57:46 -0300

    added husky

--- src/moduleStyled/MostBoughtStyled.tsx
+++ src/moduleStyled/MostBoughtStyled.tsx
@@ -5,12 +5,6 @@ export const MostBoughtContainer = styled.div`
   display: flex;
   flex-direction: column-reverse;
 
-  > :not([hidden]) ~ :not([hidden]) {
-    --tw-space-y-reverse: 0;
-    margin-top: calc(1rem * calc(1 - var(--tw-space-y-reverse)));
-    margin-bottom: calc(1rem * var(--tw-space-y-reverse));
-  }
-
   @media screen and (min-width: 768px) {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
